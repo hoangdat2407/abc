3db2b0b77193c47b19f097fd03551e8c1f30077f
hoangdat2407 <hoangdat2407@gmail.com>
2022-06-22 08:07:35 +0700

    l

--- index.js
+++ index.js
@@ -8,30 +8,50 @@ var menu = document.getElementsByClassName("menu");
     // khi nhấn 1 cái lại bị reset luôn)t hiểu rồi kk 
     // ok nhé:)
    menu[0].innerHTML = `
-   <li><a href="#home" class="list-item " onclick="menuClick(0)" ><i class="fa-solid fa-house"></i> Home</a></li>
-   <li><a href="#about" class="list-item "  onclick="menuClick(1)"><i class="fa-solid fa-angles-right"></i> About</a></li>
-   <li><a href="#contact" class="list-item " onclick="menuClick(2)"><i class="fa-solid fa-phone"></i> Contact</a></li>
-   <div  class="underline t1" ></div>
+    
+   <li><a href="#home" class="list-item " style="color:green" onclick="menuClick(0)" ><i class="fa-solid fa-house" style="color:green" ></i> Home</a></li>
+   <li><a href="#math" class="list-item " onclick="menuClick(1)"><i class="fa-solid fa-angles-right"></i> Math</a></li>
+   <li><a href="#infomatic" class="list-item "  onclick="menuClick(2)"><i class="fa-solid fa-angles-right"></i> Informatic</a></li>
+   <li><a href="#contact" class="list-item "   onclick="menuClick(3)"><i class="fa-solid fa-phone"></i> Contact</a></li>
+  <div class="underline t1"></div>
+ 
       `;
   };
   //About[0].addEventListener("click", ()=> {
   if (x == 1) {
     menu[0].innerHTML = `
-      <li><a href="#home" class="list-item " onclick="menuClick(0)" ><i class="fa-solid fa-house"></i> Home</a></li>
-          <li><a href="#about" class="list-item "  onclick="menuClick(1)"><i class="fa-solid fa-angles-right"></i> About</a></li>
-          <li><a href="#contact" class="list-item " onclick="menuClick(2)"><i class="fa-solid fa-phone"></i> Contact</a></li>
-          <div class="underline t2" ></div>
+     
+    <li><a href="#home" class="list-item " onclick="menuClick(0)" ><i class="fa-solid fa-house" ></i> Home</a></li>
+    <li><a href="#math" class="list-item "style="color:green"  onclick="menuClick(1)"><i class="fa-solid fa-angles-right"style="color:green" ></i> Math</a></li>
+    <li><a href="#infomatic" class="list-item "  onclick="menuClick(2)"><i class="fa-solid fa-angles-right"></i> Informatic</a></li>
+    <li><a href="#contact" class="list-item "   onclick="menuClick(3)"><i class="fa-solid fa-phone"></i> Contact</a></li>
+   <div class="underline t2"></div>
+  
       `;
   }
   // });
   if (x == 2) {
   // Contact[0].addEventListener("click",  ()=> {
     menu[0].innerHTML = `
-    <li><a href="#home" class="list-item " onclick="menuClick(0)" ><i class="fa-solid fa-house"></i> Home</a></li>
-          <li><a href="#about" class="list-item "  onclick="menuClick(1)"><i class="fa-solid fa-angles-right"></i> About</a></li>
-          <li><a href="#contact" class="list-item " onclick="menuClick(2)"><i class="fa-solid fa-phone"></i> Contact</a></li>
-          <div class="underline t3" ></div>
+   
+    <li><a href="#home" class="list-item " onclick="menuClick(0)" ><i class="fa-solid fa-house" ></i> Home</a></li>
+    <li><a href="#math" class="list-item " onclick="menuClick(1)"><i class="fa-solid fa-angles-right"></i> Math</a></li>
+    <li><a href="#infomatic" class="list-item " style="color:green"  onclick="menuClick(2)"><i class="fa-solid fa-angles-right"style="color:green" ></i> Informatic</a></li>
+    <li><a href="#contact" class="list-item "  onclick="menuClick(3)"><i class="fa-solid fa-phone"></i> Contact</a></li>
+   <div class="underline t3"></div>
+  
       `;
+  }
+  if(x==3)
+  {
+    menu[0].innerHTML = `  
+    <li><a href="#home" class="list-item " onclick="menuClick(0)" ><i class="fa-solid fa-house" ></i> Home</a></li>
+    <li><a href="#math" class="list-item " onclick="menuClick(1)"><i class="fa-solid fa-angles-right"></i> Math</a></li>
+    <li><a href="#infomatic" class="list-item "  onclick="menuClick(2)"><i class="fa-solid fa-angles-right"></i> Informatic</a></li>
+    <li><a href="#contact" class="list-item " style="color:green"  onclick="menuClick(3)"><i class="fa-solid fa-phone"style="color:green"></i> Contact</a></li>
+   <div class="underline t4"></div>
+  
+`;
   }
   // });
 };
